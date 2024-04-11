dbba0300931b7e7a488818e2a30f154b3ab4ad0a
adiazt01 <adiaztovar2004@gmail.com>
2024-04-11 12:29:30 -0400

    Add UrlContainer component to dashboard

---
+++ src/app/(dashboard)/dashboard/_components/container/UrlContainer.tsx
@@ -0,0 +1,15 @@
+import { getUrlsByUserId } from "../../_lib/UrlData";
+import UrlCard from "../cards/UrlCard";
+
+export default async function UrlContainer() {
+    const urlsData = await getUrlsByUserId();
+  return (
+    <ul className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full overflow-hidden">
+      {urlsData.map((url) => (
+        <li key={url.id}>
+          <UrlCard url={url} />
+        </li>
+      ))}
+    </ul>
+  );
+}
